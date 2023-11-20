eb6a2701be3d31a7268ea040bd4610043a71a991
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-20 22:11:57 +0530

    Updated Solution.cpp

--- Day 3/Solution.cpp
+++ Day 3/Solution.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> currentCombination;
 
+        sort(candidates.begin(), candidates.end());
+
+        backtrack(candidates, target, 0, currentCombination, result);
+
+        return result;
+    }
+
+private:
+    void backtrack(const vector<int>& candidates, int target, int start,
+                   vector<int>& currentCombination, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(currentCombination);
+            return;
+        }
+
+        for (int i = start; i < candidates.size() && candidates[i] <= target; ++i) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            currentCombination.push_back(candidates[i]);
+
+            backtrack(candidates, target - candidates[i], i, currentCombination, result);
+
+            currentCombination.pop_back();
+        }
+    }
+};
