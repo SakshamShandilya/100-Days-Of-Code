d94812e0cb9348502b75b0907ec76d247151c7f2
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-22 00:02:50 +0530

    Updated Solution.java

--- Day 4/Solution.java
+++ Day 4/Solution.java
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        std::vector<int> currentPermutation;
+        std::vector<bool> used(nums.size(), false); 
 
+        generatePermutations(nums, used, currentPermutation, result);
+
+        return result;
+    }
+
+private:
+    void generatePermutations(const std::vector<int>& nums, std::vector<bool>& used, std::vector<int>& currentPermutation, std::vector<std::vector<int>>& result) {
+        if (currentPermutation.size() == nums.size()) {
+            result.push_back(currentPermutation);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!used[i]) {
+                used[i] = true;
+                currentPermutation.push_back(nums[i]);
+
+                generatePermutations(nums, used, currentPermutation, result);
+
+                used[i] = false;
+                currentPermutation.pop_back();
+            }
+        }
+    }
+};
