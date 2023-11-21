5ed30afa953a1174a67ec09fd3b15c07ab24c05f
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-22 00:24:05 +0530

    Updated Solution.java

--- Day 5/Solution.java
+++ Day 5/Solution.java
@@ -1,1 +1,61 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        solveNQueensHelper(0, board, result);
+
+        return result;
+    }
+
+    private void solveNQueensHelper(int row, char[][] board, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(constructBoard(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isValidMove(board, row, col)) {
+                board[row][col] = 'Q';
+                solveNQueensHelper(row + 1, board, result);
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+    }
+
+    private boolean isValidMove(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> constructBoard(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            solution.add(new String(board[i]));
+        }
+        return solution;
+    }
+}
 
