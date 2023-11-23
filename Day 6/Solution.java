6f5a66479758069e4e96c593ddf9f703c4bb8caf
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-23 23:44:56 +0530

    Updated Solution.java

--- Day 6/Solution.java
+++ Day 6/Solution.java
@@ -1,1 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        
+
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
 
