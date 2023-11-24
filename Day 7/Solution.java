a570935b4903547ba471920a837028a2a3b6f894
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-24 23:17:27 +0530

    Updated Solution.java

--- Day 7/Solution.java
+++ Day 7/Solution.java
@@ -1,1 +1,43 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+    
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                
+                if (board[i][j] == word.charAt(0)) {
+                    
+                    if (dfs(board, i, j, word, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, int i, int j, String word, int index) {
+        
+        if (index == word.length()) {
+            return true;
+        }
+
+    
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '*';
+
+        boolean found = dfs(board, i + 1, j, word, index + 1)
+                || dfs(board, i - 1, j, word, index + 1)
+                || dfs(board, i, j + 1, word, index + 1)
+                || dfs(board, i, j - 1, word, index + 1);
+
+        
+        board[i][j] = temp;
+
+        return found;
+    }
+}
 
