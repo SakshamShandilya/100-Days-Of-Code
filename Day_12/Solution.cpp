2616146d739280598d8b8cfa369a2c81a396f842
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-29 22:25:03 +0530

    Updated Solution.cpp

--- Day_12/Solution.cpp
+++ Day_12/Solution.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    #define DPSolver ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int searchInsert(vector<int>& nums, int trgt) {
+        DPSolver;
+        int b = 0; int e = nums.size(); int m = (e+b) / 2; 
+        while (e > b){
+            if (nums[m] == trgt)
+                return m; 
+            else if (nums[m] > trgt)
+                e = m; 
+            else 
+                b = m + 1; 
+            m = (b+e)/ 2;
+        }
+        // if( m < nums.size() && m > )
+        return m; 
+    }
+};
