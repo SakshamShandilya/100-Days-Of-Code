e249afd2e380d662bb7cc16dea62070b9d02b153
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-11-30 23:11:07 +0530

    Updated Solution.java

--- Day_13/Solution.java
+++ Day_13/Solution.java
@@ -1,1 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int key) {
+        int n = arr.length;
+        int m = arr[0].length;
+        boolean res = false;
+        
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(key>=arr[mid][0] && key<=arr[mid][m-1]){
+                if(Arrays.binarySearch(arr[mid],key)>=0){
+                    res=true;
+                }
+                break;
+            }else if(key<=arr[mid][0]){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+        return res;
+    }
 
+}
