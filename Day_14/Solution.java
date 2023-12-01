86c4aa2b363b77e9670ae36641ac5425960859b0
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-01 21:39:45 +0530

    Updated Solution.java
    Working on optimising my memory sector

--- Day_14/Solution.java
+++ Day_14/Solution.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    static int max_sum;
+    public static int calc(TreeNode root){
+        if(root==null) return 0;
+        int left=calc(root.left);
+        int right=calc(root.right);
+        int root_left=root.val+left;
+        int root_right=root.val+right;
+        int all=root.val+left+right;
+        max_sum=Math.max(max_sum,Math.max(Math.max(root_left,root_right),Math.max(all,root.val)));
+        return Math.max(Math.max(root.val+left,root.val+right),root.val);
+    }
+    public int maxPathSum(TreeNode root) {
+        max_sum=Integer.MIN_VALUE;
+        calc(root);
+        int temp=max_sum;
+        max_sum=Integer.MIN_VALUE;
+        return temp;
+    }
+}
