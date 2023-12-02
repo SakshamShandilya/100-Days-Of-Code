7ec08ec201ec4a18036833a716608a6d48bd8a2f
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-02 19:22:25 +0530

    Updated Solution.java

--- Day_15/Solution.java
+++ Day_15/Solution.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int result = solution.findMin(nums);
+        System.out.println("Minimum element: " + result);
+    }
+}
 
