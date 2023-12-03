3f6ab12b655c51f580f0d6875eba0e9b21d1e79a
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-03 18:28:14 +0530

    Updated Solution.java

--- Day_16/Solution.java
+++ Day_16/Solution.java
@@ -1,1 +1,41 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
 
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        Solution solution = new Solution();
+        List<Integer> result = solution.inorderTraversal(root);
+
+        System.out.println(result);
+    }
+}
