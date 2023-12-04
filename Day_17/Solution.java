60e84eac7375043f9f5d84ee913301d3d48ca2dc
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-04 22:54:40 +0530

    Updated Solution.java

--- Day_17/Solution.java
+++ Day_17/Solution.java
@@ -1,1 +1,15 @@
+class Solution {
+    private long minVal = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true; 
+        if (!isValidBST(root.left)) return false;
+        
+        if (minVal >= root.val) return false; 
 
+        minVal = root.val;
+
+        if (!isValidBST(root.right)) return false;
+
+        return true;
+    } 
+}
