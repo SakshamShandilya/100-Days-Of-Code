c95c0a141d32efaa31253164d4d3c576d8078ed1
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-05 22:44:27 +0530

    Updated Solution.java

--- Day_18/Solution.java
+++ Day_18/Solution.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public boolean isTreeSymmetric(TreeNode leftRoot, TreeNode rightRoot){
+        if(leftRoot == null && rightRoot == null)
+            return true;
+        if((leftRoot == null && rightRoot != null)  || (leftRoot != null && rightRoot == null))
+            return false;
+        if(leftRoot.val != rightRoot.val)
+            return false;
+        return isTreeSymmetric(leftRoot.left, rightRoot.right) && isTreeSymmetric(leftRoot.right, rightRoot.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return isTreeSymmetric(root.left, root.right);
+    }
+}
