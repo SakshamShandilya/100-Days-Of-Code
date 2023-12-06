ba0ce236580106ed09eaee3aef37f39e8e7c3400
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-06 19:45:17 +0530

    Updated Solution.cpp

--- Day_19/Solution.cpp
+++ Day_19/Solution.cpp
@@ -1,1 +1,31 @@
+#include <vector>
+#include <queue>
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) {
+            return result;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> currentLevel;
 
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                currentLevel.push_back(node->val);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            result.push_back(currentLevel);
+        }
+        return result;
+    }
+};
