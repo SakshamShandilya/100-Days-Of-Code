2f537cd02bb4905959e6d31709a066687593f4a4
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-07 21:51:52 +0530

    Updated Solution.cpp

--- Day_20/Solution.cpp
+++ Day_20/Solution.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
