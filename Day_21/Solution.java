b48f828cd5f08b9698f3652de0b651aee638fb62
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-08 22:28:00 +0530

    Updated Solution.java

--- Day_21/Solution.java
+++ Day_21/Solution.java
@@ -1,1 +1,26 @@
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inorderMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        return buildTreeHelper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inorderMap);
+    }
 
+    private TreeNode buildTreeHelper(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inorderMap) {
+        if (preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+
+        int rootValue = preorder[preStart];
+        TreeNode root = new TreeNode(rootValue);
+
+        int rootIndexInInorder = inorderMap.get(rootValue);
+        int leftSubtreeSize = rootIndexInInorder - inStart;
+
+        root.left = buildTreeHelper(preorder, preStart + 1, preStart + leftSubtreeSize, inorder, inStart, rootIndexInInorder - 1, inorderMap);
+        root.right = buildTreeHelper(preorder, preStart + leftSubtreeSize + 1, preEnd, inorder, rootIndexInInorder + 1, inEnd, inorderMap);
+
+        return root;
+    }
+}
