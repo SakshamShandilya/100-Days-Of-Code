f7d72a512c6ad57cdf5dfc8ce8e5b34d438210bd
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-09 21:58:57 +0530

    Updated Solution.java

--- Day_22/Solution.java
+++ Day_22/Solution.java
@@ -1,1 +1,21 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
 
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+
+        int mid = low + (high - low) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, low, mid - 1);
+        node.right = helper(nums, mid + 1, high);
+        return node;
+    }
+}
