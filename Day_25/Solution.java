8c6cc49e140f7c930983d47286ef553c9545cee5
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-12 20:25:37 +0530

    Updated Solution.java

--- Day_25/Solution.java
+++ Day_25/Solution.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode curr = root.left;
+        root.left = root.right;
+        root.right = curr;
+        return root;
+    }
+}
