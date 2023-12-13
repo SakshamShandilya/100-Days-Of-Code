abdf165b5fe9996c7623b17af9f2db7a07df0d19
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-13 20:41:20 +0530

    Updated Solution.java

--- Day_26/Solution.java
+++ Day_26/Solution.java
@@ -1,1 +1,22 @@
+class Solution {
+    int ans=0;
+    int count=0;
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root,k);
+        return ans;
+        
+    }
+    public void inOrder(TreeNode root,int k){
+        if(root==null) return;
+        
+        inOrder(root.left,k);
+        count++;
+        if(count==k){
+            ans=root.val;
+            return;
+        }
+        inOrder(root.right,k);
 
+    }
+
+}
