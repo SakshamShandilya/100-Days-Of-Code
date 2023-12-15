6415f4af523ac414d1df037873bc84d386385354
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-15 23:47:10 +0530

    Updated Solution.java

--- Day_28/Solution.java
+++ Day_28/Solution.java
@@ -1,1 +1,25 @@
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0L, 1);
+        return dfs(root, 0L, targetSum, prefixSumCount);
+    }
 
+    private int dfs(TreeNode node, long currentSum, int targetSum, Map<Long, Integer> prefixSumCount) {
+        if (node == null) {
+            return 0;
+        }
+
+        currentSum += node.val;
+        int pathCount = prefixSumCount.getOrDefault(currentSum - targetSum, 0);
+
+        prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
+
+        pathCount += dfs(node.left, currentSum, targetSum, prefixSumCount);
+        pathCount += dfs(node.right, currentSum, targetSum, prefixSumCount);
+
+        prefixSumCount.put(currentSum, prefixSumCount.get(currentSum) - 1);
+
+        return pathCount;
+    }
+}
