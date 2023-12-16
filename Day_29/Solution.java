609c862c52d8682969d4b1fac3e415ab87988f8a
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-16 23:24:59 +0530

    Updated Solution.java

--- Day_29/Solution.java
+++ Day_29/Solution.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int diameter[] = new int[1];
+        height(root,diameter);
+      return diameter[0];
+    }
+    public int height(TreeNode root, int diameter[]){
+        if(root == null){
+            return 0;
+        }
+        int left = height(root.left,diameter);
+        int right = height(root.right,diameter);
+        diameter[0] = Math.max(diameter[0],left + right);
+        return Math.max(left,right)+1;
+    }
+}
