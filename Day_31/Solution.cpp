0b8d9f1ef72f44a830b22239a7f48419a885f867
SAKSHAM SHANDILYA <90836873+saksham140023@users.noreply.github.com>
2023-12-18 22:15:38 +0530

    Updated Solution.cpp

--- Day_31/Solution.cpp
+++ Day_31/Solution.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        if (n <= 1) return 0;
+        int ans = 0;
+        int start = -1;
+        stack<int> stk;
 
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                if (start == -1) {
+                    start = i;
+                }
+                stk.push(i);
+            } else {
+                if (stk.empty()) {
+                    start = -1;
+                } else {
+                    stk.pop();
+                    if (stk.empty()) {
+                        ans = max(ans, i - start + 1);
+                    } else {
+                        ans = max(ans, i - stk.top());
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
