cb4f3607d620809bbe4cec00273bbf14343c2acb
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-01-01 20:56:34 +0530

    Updated Solution.java

---
+++ Day_45/Leetcode/Solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int numOfIslands = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numOfIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return numOfIslands;
+    }
+    private void dfs(char[][] grid, int row, int col) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0'; 
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        Solution solution = new Solution();
+        int result = solution.numIslands(grid);
+        System.out.println("Number of Islands: " + result);
+    }
+}
