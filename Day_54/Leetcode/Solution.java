1e64dfaacbcc8b2aa801760b4857b81d59f158fc
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-01-10 23:07:18 +0530

    Updated Solution.java

---
+++ Day_54/Leetcode/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        
+        if (n == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int cnt = 1;
+        int maxi = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    cnt++;
+                } else {
+                    maxi = Math.max(maxi, cnt);
+                    cnt = 1;
+                }
+            }
+        }
+
+        return Math.max(maxi, cnt);
+    }
+}
