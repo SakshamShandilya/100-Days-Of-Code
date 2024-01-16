c80e72ba079e108eef2793754a9abe0a755332d3
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-01-16 23:25:52 +0530

    Created Solution.java

---
+++ Day_60/Geeks For Geeks/Solution.java
@@ -0,0 +1,11 @@
+class Solution{
+    static int numberSequence(int m, int n){
+        if(m < n){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        return numberSequence(m-1, n) + numberSequence(m/2, n-1);
+    }
+}
