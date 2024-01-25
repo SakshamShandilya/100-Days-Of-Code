8c50bb9f053b9ea4853583882ef6f08b1a27ce15
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-01-25 17:28:48 +0530

    Created Solution.cpp

---
+++ Day_69/Leetcode/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int length(ListNode *head){
+        int len = 0;
+        while(head){
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB) return NULL;
+        int lenA = length(headA), lenB = length(headB);
+        if(lenA>lenB){
+            while(lenA>lenB){
+                headA = headA->next;
+                lenA--;
+            }
+        }
+        else if(lenA<lenB){
+            while(lenA<lenB){
+                headB = headB->next;
+                lenB--;
+            }
+        }
+
+        while(headA && headB){
+            if(headA==headB) return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
