56a2107f8f311ae57ba0d5e55e13d4198213389f
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-01-31 23:40:47 +0530

    Updated Solution.java

---
+++ Day_75/Leetcode/Solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    private boolean binarySearch(int[][] matrix, int target, int start, boolean vertical) {
+        int lo = start;
+        int hi = vertical ? matrix[0].length-1 : matrix.length-1;
+
+        while (hi >= lo) {
+            int mid = (lo + hi)/2;
+            if (vertical) {
+                if (matrix[start][mid] < target) {
+                    lo = mid + 1;
+                } else if (matrix[start][mid] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            } else {
+                if (matrix[mid][start] < target) {
+                    lo = mid + 1;
+                } else if (matrix[mid][start] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int shorterDim = Math.min(matrix.length, matrix[0].length);
+        for (int i = 0; i < shorterDim; i++) {
+            boolean verticalFound = binarySearch(matrix, target, i, true);
+            boolean horizontalFound = binarySearch(matrix, target, i, false);
+            if (verticalFound || horizontalFound) {
+                return true;
+            }
+        }
+        
+        return false; 
+    }
+}
