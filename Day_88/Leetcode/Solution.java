cc31241d478743c438bf10621c77f3efe3c5d1ee
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-02-13 22:44:00 +0530

    Updated Solution.java

---
+++ Day_88/Leetcode/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 1) {
+            return;
+        }
+
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
+            }
+        }
+
+       
+         while(nonZeroIndex<nums.length){
+            nums[nonZeroIndex] = 0;
+            nonZeroIndex++;
+        }
+    }
+}
