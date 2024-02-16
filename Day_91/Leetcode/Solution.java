d89d9f802a6e4b908b197c58d68f97b7b43f9a43
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-02-17 00:02:22 +0530

    Updated Solution.java

---
+++ Day_91/Leetcode/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+
+        int count = 0;
+
+
+        for(int j = 0; j < nums.length; j++) {
+
+            if(nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
