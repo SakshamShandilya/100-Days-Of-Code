673f35690e6e907858d182b8b5e632f991c04af4
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-02-18 22:17:51 +0530

    Created Solution.java

---
+++ Day_93/Geeks For Geeks/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int sumOfLeafNodes(Node root) {
+        if(root == null)
+          return 0;
+        if(root.left == null && root.right == null)
+          return root.data;
+        return sumOfLeafNodes(root.left)+sumOfLeafNodes(root.right);
+    }
+}
