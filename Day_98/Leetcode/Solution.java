0e73566a582c51ee8c21e3e053b84094a4847ae2
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-02-23 23:35:11 +0530

    Updated Solution.java

---
+++ Day_98/Leetcode/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int numsLength = nums.length;
+        int prefixProduct = 1;
+        int suffixProduct = 1;
+        int[] result = new int[numsLength];
+        for(int i = 0; i < numsLength; i++) {
+            result[i] = prefixProduct;
+            prefixProduct *= nums[i];
+        }
+        for(int i = numsLength-1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+        return result;
+    }
+}
