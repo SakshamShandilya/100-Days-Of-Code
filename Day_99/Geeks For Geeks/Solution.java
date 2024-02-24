1d6884fd6aa57fea9d4cac363ee11bd30d0e608d
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-02-24 22:39:13 +0530

    Created Solution.java

---
+++ Day_99/Geeks For Geeks/Solution.java
@@ -0,0 +1,13 @@
+class Solution{
+    public int maxSum(int n) { 
+            if(n<2)
+                return n;
+            int dp[] = new int[n+1];
+            dp[0] = 0;
+            dp[1] = 1;
+            for (int i=2; i<=n; i++)
+               dp[i] = Math.max(dp[i/2] + dp[i/3] + dp[i/4], i);
+         
+            return dp[n];
+    } 
+}
