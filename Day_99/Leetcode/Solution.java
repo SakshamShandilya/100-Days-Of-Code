a1205ce34716bc40c36ec3ba0ae5e9618064ac7a
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-02-24 22:47:50 +0530

    Updated Solution.java

---
+++ Day_99/Leetcode/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+        public static int findDuplicate(int[] nums) {
+        int len = nums.length;
+        int[] cnt = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            cnt[nums[i]]++;
+            if (cnt[nums[i]] > 1) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+}    
